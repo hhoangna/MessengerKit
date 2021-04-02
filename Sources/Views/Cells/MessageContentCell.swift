85f7b37eb6cd93dc7c10e22ddf2af7fca09bf2d3
Rum <rum@localhost>
2021-04-02 17:29:00 +0700

    Update layout status

--- Sources/Views/Cells/MessageContentCell.swift
+++ Sources/Views/Cells/MessageContentCell.swift
@@ -52,7 +52,6 @@ open class MessageContentCell: MessageCollectionViewCell, UIGestureRecognizerDel
         label.numberOfLines = 1
         label.textAlignment = .center
         label.backgroundColor = .white
-        label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
     }()
@@ -643,17 +642,13 @@ open class MessageContentCell: MessageCollectionViewCell, UIGestureRecognizerDel
         cellTopLabel.textAlignment = attributes.cellTopLabelAlignment.textAlignment
         cellTopLabel.textInsets = attributes.cellTopLabelAlignment.textInsets
         
+        cellTopLabel.frame = CGRect(origin: .zero, size: attributes.cellTopLabelSize)
+                
         NSLayoutConstraint.activate([
-            cellTopLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            cellTopLabel.heightAnchor.constraint(equalToConstant: attributes.cellTopLabelSize.height),
-            cellTopLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
-            cellTopLabel.bottomAnchor.constraint(equalTo: messageTopLabel.topAnchor),
-            
             sparateTopLine.heightAnchor.constraint(equalToConstant: 0.5),
             sparateTopLine.centerYAnchor.constraint(equalTo: cellTopLabel.centerYAnchor),
             sparateTopLine.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             sparateTopLine.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16)
-            
         ])
         sparateTopLine.isHidden = attributes.cellTopLabelSize.height == 0
     }
