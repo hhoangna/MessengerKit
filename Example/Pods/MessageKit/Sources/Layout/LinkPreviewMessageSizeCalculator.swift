4b1feb27ad737bf85061af32a4a40c20b15bbddb
Rum <rum@localhost>
2021-04-06 09:13:02 +0700

    Update

--- Example/Pods/MessageKit/Sources/Layout/LinkPreviewMessageSizeCalculator.swift
+++ Example/Pods/MessageKit/Sources/Layout/LinkPreviewMessageSizeCalculator.swift
@@ -56,7 +56,7 @@ open class LinkPreviewMessageSizeCalculator: TextMessageSizeCalculator {
         }
     }
 
-    open override func messageContainerSize(for message: MessageType) -> CGSize {
+    open override func messageContainerSize(for message: MessageType, maxWidth: CGFloat = 0) -> CGSize {
         guard case MessageKind.linkPreview(let linkItem) = message.kind else {
             fatalError("messageContainerSize received unhandled MessageDataType: \(message.kind)")
         }
